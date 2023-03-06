d52f841be0acf5cbf9d8f161a2381e779b198386
Hassaan Junaid <hassaanjunaid.soomro@gmail.com>
2023-03-06 13:41:30 -0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:i_connect/firebase_options.dart';
 
 import 'app/services/local_storage.dart';
 // import 'app/util/dependency.dart';
@@ -8,6 +10,7 @@ import 'presentation/app.dart';
 void main() async {
   // DependencyCreator.init();
   WidgetsFlutterBinding.ensureInitialized();
+  await initFireBaseServices();
   await initServices();
   runApp(App());
 }
@@ -16,4 +19,10 @@ initServices() async {
   print('starting services ...');
   await Get.putAsync(() => LocalStorageService().init());
   print('All services started...');
+}
+
+initFireBaseServices() async {
+  print('starting Firebase Initialization');
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print('firebase initialization done');
 }
